d5d59f753262e404cd466e1bd2141d0d38379d6e
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-06-29 15:16:32 +1000

    Allow the console to approve players

--- src/main/java/net/antlertech/slicerslicemanager/commands/approveCommand.java
+++ src/main/java/net/antlertech/slicerslicemanager/commands/approveCommand.java
@@ -3,6 +3,7 @@ package net.antlertech.slicerslicemanager.commands;
 import net.antlertech.slicerslicemanager.SQL.SQLUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,27 +12,26 @@ import net.antlertech.slicerslicemanager.models.messages;
 public class approveCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < args.length; i++) {
-                builder.append(args[i]);
-                builder.append(" ");
-            }
-            String approveTargetInput = builder.toString();
-            approveTargetInput = approveTargetInput.stripTrailing();
-            Player approveTargetPlayerObject = Bukkit.getServer().getPlayer(approveTargetInput);
-            p.sendMessage("Approving " + approveTargetPlayerObject.getName());
-            boolean approved = SQLUtil.approveSlice(approveTargetPlayerObject.getUniqueId());
-            if (!approved) {
-                p.sendMessage(messages.getErrorApprovingSliceMessage());
-                return true;
-            }
-            approveTargetPlayerObject.sendMessage(messages.getYourSliceApprovedMessage());
-            p.sendMessage(messages.getApprovedSliceMessage(approveTargetPlayerObject));
+        if (args.length == 0) {
+            sender.sendMessage(messages.getNoUserSpecifiedMessage());
             return true;
         }
-        sender.sendMessage(messages.getConsoleMessage());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            builder.append(args[i]);
+            builder.append(" ");
+        }
+        String approveTargetInput = builder.toString();
+        approveTargetInput = approveTargetInput.stripTrailing();
+        Player approveTargetPlayerObject = Bukkit.getServer().getPlayer(approveTargetInput);
+        sender.sendMessage("Approving " + approveTargetPlayerObject.getName());
+        boolean approved = SQLUtil.approveSlice(approveTargetPlayerObject.getUniqueId());
+        if (!approved) {
+            sender.sendMessage(messages.getErrorApprovingSliceMessage());
+            return true;
+        }
+        approveTargetPlayerObject.sendMessage(messages.getYourSliceApprovedMessage());
+        sender.sendMessage(messages.getApprovedSliceMessage(approveTargetPlayerObject));
         return true;
     }
 }
--- src/main/java/net/antlertech/slicerslicemanager/models/messages.java
+++ src/main/java/net/antlertech/slicerslicemanager/models/messages.java
@@ -4,28 +4,27 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class messages {
+    private static final String yourSliceApprovedMessage = ChatColor.GREEN + "" + ChatColor.BOLD + "YAY! " + ChatColor.GRAY + "Your slice has been approved!";
     private static final String sliceClaimedMessage = ChatColor.GREEN + "" + ChatColor.BOLD + "YAY! " + ChatColor.GRAY + "You have claimed a slice!";
     private static final String sliceIsAlreadyClaimedMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "This slice is already claimed!";
     private static final String playerNotAllowedToClaimNewSliceMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "You are not allowed to claim a slice new slice! Please get a staff member to approve it for you!";
     private static final String notTheirSliceMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "That is not your slice!";
     private static final String bannedBlockMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "That is an illegal block!";
+    private static final String consoleMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "Do Not Run This Command As Console!";
+    private static final String noUserSpecifiedMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "You need to specify a user!";
     private static final String SQLErrorMessage = ChatColor.RED + "" + ChatColor.BOLD + "Hmm... " + ChatColor.GRAY + "There was an error with the SQL database! Please try again!";
     private static final String errorClaimingSliceMessage = ChatColor.RED + "" + ChatColor.BOLD + "Hmm... " + ChatColor.GRAY + "There was an error claiming your slice! Please try again!";
     private static final String errorApprovingSliceMessage = ChatColor.RED + "" + ChatColor.BOLD + "Hmm... " + ChatColor.GRAY + "There was an error approving the slice! Please try again!";
-    private static final String yourSliceApprovedMessage = ChatColor.GREEN + "" + ChatColor.BOLD + "YAY! " + ChatColor.GRAY + "Your slice has been approved!";
-    private static final String consoleMessage = ChatColor.RED + "" + ChatColor.BOLD + "HEY! " + ChatColor.GRAY + "This slice is already claimed!";
 
+    public static String getApprovedSliceMessage(Player player) {
+        String approvedSliceMessage = ChatColor.GREEN + "" + ChatColor.BOLD + "YAY! " + ChatColor.GRAY + "You have approved " + player.getName() + " slice!";
+        return approvedSliceMessage;
+    }
+    public static String getYourSliceApprovedMessage() {
+        return yourSliceApprovedMessage;
+    }
     public static String getSliceClaimedMessage() {
         return sliceClaimedMessage;
-    }
-    public static String getConsoleMessage() {
-        return consoleMessage;
-    }
-    public static String getErrorClaimingSliceMessage() {
-        return errorClaimingSliceMessage;
-    }
-    public static String getErrorApprovingSliceMessage() {
-        return errorApprovingSliceMessage;
     }
     public static String getSliceIsAlreadyClaimedMessage() {
         return sliceIsAlreadyClaimedMessage;
@@ -36,17 +35,22 @@ public class messages {
     public static String getBannedBlockMessage() {
         return bannedBlockMessage;
     }
-    public static String getSQLErrorMessage() {
-        return SQLErrorMessage;
-    }
     public static String getPlayerNotAllowedToClaimNewSliceMessage() {
         return playerNotAllowedToClaimNewSliceMessage;
     }
-    public static String getYourSliceApprovedMessage() {
-        return yourSliceApprovedMessage;
+    public static String getConsoleMessage() {
+        return consoleMessage;
     }
-    public static String getApprovedSliceMessage(Player player) {
-        String approvedSliceMessage = ChatColor.GREEN + "" + ChatColor.BOLD + "YAY! " + ChatColor.GRAY + "You have approved " + player.getName() + " slice!";
-        return approvedSliceMessage;
+    public static String getNoUserSpecifiedMessage() {
+        return noUserSpecifiedMessage;
+    }
+    public static String getSQLErrorMessage() {
+        return SQLErrorMessage;
+    }
+    public static String getErrorClaimingSliceMessage() {
+        return errorClaimingSliceMessage;
+    }
+    public static String getErrorApprovingSliceMessage() {
+        return errorApprovingSliceMessage;
     }
 }
