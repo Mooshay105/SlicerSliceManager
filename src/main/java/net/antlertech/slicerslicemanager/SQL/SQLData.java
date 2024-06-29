1c610c398293813373bc556e3de6bd705fd0c3a0
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-06-29 14:23:07 +1000

    Get Ready for the Beautiful Bellbird Update and add Full changeability to the login and password for the DB

--- src/main/java/net/antlertech/slicerslicemanager/SQL/SQLData.java
+++ src/main/java/net/antlertech/slicerslicemanager/SQL/SQLData.java
@@ -5,10 +5,10 @@ import net.antlertech.slicerslicemanager.SlicerSliceManager;
 public class SQLData {
     private static final SlicerSliceManager plugin = SlicerSliceManager.getPlugin();
 
-    private static final String host = "localhost";
-    private static final int port = 3306;
-    private static final String database = "SlicesDatabase";
-    private static final String username = "SlicerSlices";
+    private static final String host = plugin.getConfig().getString("host");
+    private static final int port = plugin.getConfig().getInt("port");
+    private static final String database = plugin.getConfig().getString("database");
+    private static final String username = plugin.getConfig().getString("username");
     private static final String password = plugin.getConfig().getString("password");
 
     public static String getHost() {
